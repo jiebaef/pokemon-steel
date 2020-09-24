62924a065bb9d8cd5d5c621023a34f4f44cf3f35
Finn <zockeremail2@yahoo.de>
2020-09-24 19:40:39 +0200

    initial commit

---
+++ Pokemon Steel/Assets/Scripts/Editor/GroupCommand.cs
@@ -0,0 +1,16 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class GroupCommand : MonoBehaviour
+{
+    [MenuItem("GameObject/Group Selected %g")]
+    private static void GroupSelected()
+    {
+        if (!Selection.activeTransform) return;
+        var go = new GameObject(Selection.activeTransform.name + " Group");
+        Undo.RegisterCreatedObjectUndo(go, "Group Selected");
+        go.transform.SetParent(Selection.activeTransform.parent, false);
+        foreach (var transform in Selection.transforms) Undo.SetTransformParent(transform, go.transform, "Group Selected");
+        Selection.activeGameObject = go;
+    }
+}
