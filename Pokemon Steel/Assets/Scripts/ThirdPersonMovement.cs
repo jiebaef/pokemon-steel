4389f8cd1bb7164ac86935f534afc779d5061516
Finn <zockeremail2@yahoo.de>
2020-09-24 20:32:20 +0200

    added basic camera and simple movement

---
+++ Pokemon Steel/Assets/Scripts/ThirdPersonMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonMovement : MonoBehaviour
+{
+    public CharacterController controller;
+
+    public float speed = 6f;
+
+    void Update()
+    {
+        var horizontal = Input.GetAxisRaw("Horizontal");
+        var vertical = Input.GetAxisRaw("Vertical");
+
+        var direction = new Vector3(horizontal, 0, vertical).normalized;
+
+        if(direction.magnitude >= 0.1)
+        {
+            controller.Move(direction * speed * Time.deltaTime);
+        }
+    }
+}
